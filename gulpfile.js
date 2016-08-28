d5b2bbff5765817a6bcf1098ec059d13c89ced88
adam <adam.thorpe.g@gmail.com>
2016-08-27 18:01:08 -0600

    Update build process to include mixins in dist.

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,24 @@ const less = require('gulp-less');
 
 const paths = {
   entry: 'src/core.less',
-  styles: 'src/**/*.less'
+  styles: 'src/**/*.less',
+  mixins: 'src/mixins/**/*.less'
 }
 
-gulp.task('build', () =>
+gulp.task('build-styles', () =>
   gulp.src(paths.entry)
     .pipe(less())
     .pipe(gulp.dest('dist'))
 );
 
-gulp.task('watch', () =>
-  gulp.watch(paths.styles, ['build'])
-);
+gulp.task('build-mixins', () => {
+  gulp.src(paths.mixins)
+    .pipe(gulp.dest('dist/mixins'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.styles, ['build-styles']);
+  gulp.watch(paths.mixins, ['build-mixins']);
+});
+
+gulp.task('build', ['build-styles', 'build-mixins']);
